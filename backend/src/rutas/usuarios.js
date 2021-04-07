f9a687e7bb667b5514feab2a13eecb67f334ba63
Adrian Garzon <adriangarzonpozo@gmail.com>
2021-04-07 21:02:19 +0200

    Reorganización de carpetas

--- backend/src/rutas/usuarios.js
+++ backend/src/rutas/usuarios.js
@@ -21,4 +21,5 @@ router.get('/imagen/:imagen', UsuariosControlador.recuperarImagen);
 
 router.post('/seguir/:id/:seguir', UsuariosControlador.seguir); //Seguir a otro usuario
 
+
 module.exports=router;
