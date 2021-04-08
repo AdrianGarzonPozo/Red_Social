6ddbcbe1ea958a8f8c0e12be8e4ff4b9d1ec2568
Adrian Garzon <adriangarzonpozo@gmail.com>
2021-04-08 10:19:22 +0200

    API: reemplazar foto_peril de un usuario por la que ya tenia

--- backend/src/controladores/usuarios.js
+++ backend/src/controladores/usuarios.js
@@ -2,6 +2,7 @@ var usuarioModelo = require('../modelos/usuarios');
 var fs = require('fs');
 var path = require('path');
 const multer = require('multer');
+const { send } = require('process');
 
 async function recuperarTodos(req, res) {
     try {
@@ -105,61 +106,48 @@ async function subirImagen(req, res) {
         var foto_perfil = '';
 
         const storage = multer.diskStorage({
-            destination: path.join(__dirname, '../public/uploads/foto_perfil'),
+            destination: path.join(__dirname, '../public/uploads/foto_perfil/'),
             filename: (req, file, res) => {
-                foto_perfil = req.params.id + path.extname(file.originalname).toLowerCase();
+                foto_perfil = req.params.id + ".jpg";
                 res(null, foto_perfil);
             }
         });
         const multerMiddleware = multer({            //Subida de imagenes
             storage: storage,
-            dest: path.join(__dirname, '../public/uploads/foto_perfil'),
-            limits: { fileSize: 30000000 },             //Tamaño maximo 30Mb
+            dest: path.join(__dirname, '../public/uploads/foto_perfil/'),
+            limits: { fileSize: 30 * 1024 * 1024 },  //Tamaño maximo 30Mb, se borra la imagen guardada si supero los Mb SOLUCIONAR
             fileFilter: (req, file, res) => {
                 const extValidas = /jpeg|jpg|png/;
                 const mimetype = extValidas.test(file.mimetype); //Compruebo la extension
                 const extension = extValidas.test(path.extname(file.originalname));
 
-                if (mimetype && extension) {
-                    console.log(req.params.id);
-                    console.log(path.extname(file.originalname).toLowerCase());
+                if (mimetype && extension) return res(null, true);
 
-                    //PARA BORRAR LA FOTO ANTERIOR
-                    /* usuarioModelo.findById(req.params.id, (error, usuario) => {
-                        console.log(usuario);
-                        fs.unlinkSync(path.join(__dirname, '../public/uploads/foto_perfil/' + req.params.id + path.extname(file.originalname).toLowerCase()));
-                        console.log("2");
-                    }); */
-
-                    console.log("1");
-                    return res(null, true);
-                }
-                return res('Extension');
+                return res('EXT_FAILED');
             }
         }).single('foto_perfil');  //Nombre del input desde donde se subira la imagen
 
         multerMiddleware(req, res, (err) => {
             if (err) {
-                return res.send({ status: err });
+                if(err=="EXT_FAILED") return res.status(500).send({ status: "EXT_FAILED" });
+                return res.status(500).send({status:"LIMIT_SIZE"});
             }
             idUsuario = req.params.id;
             var update = { foto_perfil: foto_perfil };
 
 
-            return res.send(prueba());
+            prueba();
             async function prueba(req, res) {
                 await usuarioModelo.findByIdAndUpdate(idUsuario, update, { new: true }, (error, usuarioModificado) => {
 
-                    console.log("sdfsd");
                     if (error) return res.status(500).send({ status: 'failed' });
 
                     if (!usuarioModificado) return res.send(404).send({ status: '404' });
 
-                    /* return res.status(200).send(usuarioModificado); */
-                    return "hola";
                 });
             }
 
+            return res.send({status:'success'});
         });
 
 
